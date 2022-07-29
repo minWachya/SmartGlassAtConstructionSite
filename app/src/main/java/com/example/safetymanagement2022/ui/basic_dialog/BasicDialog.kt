68b32474fc6006228b2ef5e9d977807440eeb9d4
minyoung <minwachya@gmail.com>
2022-07-29 13:33:13 +0900

    [modify]베이직 다이얼로그 버튼 클릭 리스너 작업중(#5)

--- app/src/main/java/com/example/safetymanagement2022/ui/basic_dialog/BasicDialog.kt
+++ app/src/main/java/com/example/safetymanagement2022/ui/basic_dialog/BasicDialog.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import com.example.safetymanagement2022.databinding.DialogBasicBinding
 
@@ -36,6 +37,9 @@ class BasicDialog (val title: String, val content: String, val btn1: String, val
 
         return view
     }
+
+    fun setBtn1ClickListener(listener: View.OnClickListener) = binding.customTvBtn1.setOnClickListener(listener)
+    fun setBtn12ClickListener(listener: View.OnClickListener) = binding.customTvBtn2.setOnClickListener(listener)
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/safetymanagement2022/ui/glass_create/GlassCreateActivity.kt
+++ app/src/main/java/com/example/safetymanagement2022/ui/glass_create/GlassCreateActivity.kt
@@ -1,9 +1,11 @@
 package com.example.safetymanagement2022.ui.glass_create
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.example.safetymanagement2022.databinding.ActivityGlassCreateBinding
+import com.example.safetymanagement2022.ui.basic_dialog.BasicDialog
 
 class GlassCreateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGlassCreateBinding
@@ -26,7 +28,12 @@ class GlassCreateActivity : AppCompatActivity() {
 
     private fun setFinishBtnListener() {
         binding.btnFinish.setOnClickListener {
-
+            val glassName = binding.editGlassName.text
+            val dialog = BasicDialog("스마트 글래스 추가 완료", "‘$glassName’가 정상적으로  추가되었습니다.",
+            "", "확인")
+            dialog.setBtn1ClickListener{this@GlassCreateActivity.finish()}
+            dialog.setBtn12ClickListener { this@GlassCreateActivity.finish() }
+            dialog.show(supportFragmentManager, "CustomDialog")
         }
     }
 
