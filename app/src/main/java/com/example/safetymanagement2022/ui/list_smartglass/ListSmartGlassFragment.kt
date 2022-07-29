8701806f5bd398bb81afc8cda94728a2eefbbfc3
minyoung <minwachya@gmail.com>
2022-07-29 11:13:52 +0900

    [feat]글래스 추가 페이지로 이동(#4)

--- app/src/main/java/com/example/safetymanagement2022/ui/list_smartglass/ListSmartGlassFragment.kt
+++ app/src/main/java/com/example/safetymanagement2022/ui/list_smartglass/ListSmartGlassFragment.kt
@@ -1,5 +1,6 @@
 package com.example.safetymanagement2022.ui.list_smartglass
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,7 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.safetymanagement2022.databinding.FragmentListSmartglassBinding
+import com.example.safetymanagement2022.ui.building_create.BuildingCreateActivity
 import com.example.safetymanagement2022.ui.common.MyViewModelFactory
+import com.example.safetymanagement2022.ui.glass_create.GlassCreateActivity
 
 class ListSmartGlassFragment: Fragment() {
     private lateinit var binding: FragmentListSmartglassBinding
@@ -27,12 +30,21 @@ class ListSmartGlassFragment: Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         val admin = viewModel.listSmartGlassData.value?.admin ?: 0
         binding.admin = admin
+        binding.viewModel = viewModel
 
         viewModel.listSmartGlassData.observe(viewLifecycleOwner) { data ->
             binding.rvListSmartglass.adapter = ListSmartGlassAdapter(admin).apply {
                 submitList(data.glassList)
             }
         }
+        viewModel.openCreateGlassEvent.observe(viewLifecycleOwner) {
+            openCreateGlass()
+        }
+    }
+
+    private fun openCreateGlass() {
+        val intent = Intent(context, GlassCreateActivity::class.java)
+        startActivity(intent)
     }
 
 }
