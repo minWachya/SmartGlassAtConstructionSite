5129ea7b007bba85eb74e2798a6557066c45281c
minWachya <alal7272@naver.com>
2022-09-08 14:13:32 +0900

    [feat] s3 muili upload 기능 생성(#10)

--- app/src/main/java/com/example/safetymanagement2022/common/MultiUploaderS3Client.kt
+++ app/src/main/java/com/example/safetymanagement2022/common/MultiUploaderS3Client.kt
@@ -1,15 +1,28 @@
 package com.example.safetymanagement2022.common
 
+import android.content.Context
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
+import android.util.Log
+import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.regions.Region
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3Client
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 import java.io.File
+import java.net.URL
 
 class MultiUploaderS3Client(private val bucketName:String) {
-    fun uploadMultiple(fileToKeyUploads: MutableMap<String, File>, transferUtility: TransferUtility): Completable? {
+
+    val arrImage = mutableMapOf<String, String>()
+
+    fun uploadMultiple(fileToKeyUploads: MutableMap<String,File>, transferUtility: TransferUtility): Completable {
         return transferUtility(transferUtility)
             .flatMapCompletable { transferUtility ->
                 Observable.fromIterable(fileToKeyUploads.entries)
@@ -22,6 +35,16 @@ class MultiUploaderS3Client(private val bucketName:String) {
                     }
             }
     }
+//    fun downloadMultiple(fileToKeyUploads: MutableMap<String, File>, transferUtility: TransferUtility): Completable {
+//        return transferUtility(transferUtility)
+//            .flatMapCompletable {
+//                Observable.fromIterable(fileToKeyUploads.entries)
+//                    .flatMapCompletable { entry ->
+//                        Log.d("mmm multi", entry.key.toString())
+//                        downloadSingle(entry.key)
+//                    }
+//            }
+//    }
 
     private fun transferUtility(transferUtility: TransferUtility): Single<TransferUtility?> {
         return Single.create { emitter ->
@@ -31,37 +54,41 @@ class MultiUploaderS3Client(private val bucketName:String) {
         }
     }
 
+
     private fun uploadSingle(
         transferUtility: TransferUtility?,
         aLocalFile: File?,
         toRemoteKey: String?
-    ): Completable {
+    ): Completable? {
         return Completable.create { emitter ->
             transferUtility?.upload(bucketName,toRemoteKey, aLocalFile)
                 ?.setTransferListener(object : TransferListener {
-                    override fun onStateChanged(
-                        id: Int,
-                        state: TransferState
-                    ) {
-                        if (TransferState.FAILED == state) {
-                            emitter.onError(Exception("Transfer state was FAILED."))
-                        } else if (TransferState.COMPLETED == state) {
-                            emitter.onComplete()
+                    override fun onStateChanged(id: Int, state: TransferState?) {
+                        if (state == TransferState.COMPLETED) {
+//                            val s3Url: URL = s3Client.getUrl("detectus/$USER_ID/photo", fileName)
+//                            arrImage[index].imageUrl = s3Url.toString()]
                         }
                     }
-
-                    override fun onProgressChanged(
-                        id: Int,
-                        bytesCurrent: Long,
-                        bytesTotal: Long
-                    ) {
+                    override fun onProgressChanged(id: Int, bytesCurrent: Long, bytesTotal: Long) {
+                        val done = bytesCurrent / bytesTotal * 100.0
+                        Log.d(TAG, "UPLOAD - - ID: $id, percent done = $done")
                     }
-
-                    override fun onError(id: Int, exception: Exception) {
-                        emitter.onError(exception)
+                    override fun onError(id: Int, ex: Exception?) {
+                        Log.d(TAG, "UPLOAD ERROR - - ID: $id - - EX:" + ex.toString());
                     }
                 })
         }
     }
 
+//    private fun downloadSingle(
+//        toRemoteKey: String?
+//    ): Completable {
+//        Log.d("mmm single url", toRemoteKey.toString())
+//        return Completable.create { emitter ->
+//            val s3Url: URL = s3Client.getUrl(bucketName, toRemoteKey)
+//            arrImage[toRemoteKey.toString()] = s3Url.toString()
+//            Log.d("mmm single url", s3Url.toString())
+//            emitter.onComplete()
+//        }
+//    }
 }
--- app/src/main/java/com/example/safetymanagement2022/ui/list/buildingcreate/BuildingCreate2Fragment.kt
+++ app/src/main/java/com/example/safetymanagement2022/ui/list/buildingcreate/BuildingCreate2Fragment.kt
@@ -3,7 +3,6 @@ package com.example.safetymanagement2022.ui.list.buildingcreate
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
@@ -13,9 +12,12 @@ import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.mobile.auth.core.internal.util.ThreadUtils.runOnUiThread
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferNetworkLossHandler
 import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
 import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
@@ -27,6 +29,7 @@ import com.example.safetymanagement2022.model.FloorPlanData
 import com.example.safetymanagement2022.ui.base.BaseFragment
 import com.example.safetymanagement2022.ui.common.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.schedulers.Schedulers
 import java.io.File
 
 // 다이얼로그에서 값 받아오기 위한 인터페이스
@@ -47,7 +50,6 @@ class BuildingCreate2Fragment : BaseFragment<FragmentBuildingCreate2Binding>(R.l
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
 
-        val buildingId = requireArguments().getString(KEY_BUILDING_ID)
         val floorMax = requireArguments().getString(KEY_BUILDING_FLOOR_MAX)?.toIntOrNull() ?: 0
         val floorMin = requireArguments().getString(KEY_BUILDING_FLOOR_MIN)?.toIntOrNull() ?: 0
 
@@ -89,7 +91,6 @@ class BuildingCreate2Fragment : BaseFragment<FragmentBuildingCreate2Binding>(R.l
             val path = getRealPathFromURI(arrImage[i].imageUri!!).toString()
             val file = File(path)
             multiUploadHashMap[file.name] = file
-            //uploadImageToS3(file.name, file, i)
         }
         uploadImageToS3(multiUploadHashMap)
     }
@@ -108,13 +109,12 @@ class BuildingCreate2Fragment : BaseFragment<FragmentBuildingCreate2Binding>(R.l
 
     @SuppressLint("CheckResult")
     private fun uploadImageToS3(map: Map<String, File>) {
-        val ai: ApplicationInfo = requireContext().packageManager
+        val buildingId: Int = requireArguments().getInt(KEY_BUILDING_ID)
+        Log.d("mmm building id", buildingId.toString())
+        val ai = requireContext().packageManager
             .getApplicationInfo(requireContext().packageName, PackageManager.GET_META_DATA)
         val ak = ai.metaData["accessKey"].toString()
         val sak = ai.metaData["secretAccessKey"].toString()
-        Log.d("mmm ak", ak)
-
-        val buildingId = requireArguments().getString(KEY_BUILDING_ID)
 
         val wsCredentials = BasicAWSCredentials(ak, sak)
         val s3Client = AmazonS3Client(wsCredentials, Region.getRegion(Regions.AP_NORTHEAST_2))
@@ -123,13 +123,22 @@ class BuildingCreate2Fragment : BaseFragment<FragmentBuildingCreate2Binding>(R.l
             .context(requireContext())
             .build()
 
-//        MultiUploaderS3Client("detectus/$USER_ID/$buildingId/photo").uploadMultiple(map as MutableMap<String, File>, transferUtility)
-//            ?.subscribeOn(Schedulers.io())
-//            ?.observeOn(Schedulers.io())
-//            ?.subscribe {
-//                Runnable { callback.onComplete(_Result.Success<String>("uploaded successfully")) }
-//                handler.post(returnCallback)
-//            }
+        TransferNetworkLossHandler.getInstance(requireContext())
+
+        MultiUploaderS3Client("detectus/${viewModel.getUserId()}/$buildingId")
+            .uploadMultiple(map as MutableMap<String, File>, transferUtility)
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe {
+                runOnUiThread {
+                    Toast.makeText(context, "Uploading completed", Toast.LENGTH_LONG).show()
+                }
+            }
+
+//        MultiUploaderS3Client("detectus/${viewModel.getUserId()}/$buildingId", requireContext())
+//            .downloadMultiple(map)
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(Schedulers.io())
     }
 
     private fun getRealPathFromURI(contentUri: Uri): String? {
@@ -156,19 +165,17 @@ class BuildingCreate2Fragment : BaseFragment<FragmentBuildingCreate2Binding>(R.l
         super.onActivityResult(requestCode, resultCode, data)
 
         if (resultCode == Activity.RESULT_OK) {
-            if (Build.VERSION.SDK_INT >= 19) {
-                val uri = data?.data    // 선택한 이미지의 주소
-                if (uri != null) {
-                    arrImage[requestCode].imageUri = uri
-                    // '완료' 버튼 활성화 체크
-                    checkBtnEnable()
-                    // 이미지 색 변경
-                    (binding.rvFloorPlan.adapter as BuildingCreateAdapter)
-                        .arrImgView[requestCode]
-                        .setImageResource(R.drawable.ic_image_on)
-                }
-                else bitmap = null
+            val uri = data?.data    // 선택한 이미지의 주소
+            if (uri != null) {
+                arrImage[requestCode].imageUri = uri
+                // '완료' 버튼 활성화 체크
+                checkBtnEnable()
+                // 이미지 색 변경
+                (binding.rvFloorPlan.adapter as BuildingCreateAdapter)
+                    .arrImgView[requestCode]
+                    .setImageResource(R.drawable.ic_image_on)
             }
+            else bitmap = null
         }
     }
 
